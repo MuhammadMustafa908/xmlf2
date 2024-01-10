c931a5e40df1c8b6c9a1482955cc25cb10b69d7e
Muhammad Mustafa <muhammadmustafa1082006@gmail.com>
2024-01-10 18:18:02 +0500

    done

---
+++ one.js
@@ -0,0 +1,19 @@
+var organization = [
+    {
+        "id" : 0,
+        "name" : "abc",
+        "designation" : "Manager",
+        "salary" : 50000,
+        "phone-no" : 5698745
+    },
+
+    {
+        "id" : 1,
+        "name" : "xyz",
+        "designation" : "General Manager",
+        "salary" : 60000,
+        "phone-no" : 525484
+    },
+
+    
+]
